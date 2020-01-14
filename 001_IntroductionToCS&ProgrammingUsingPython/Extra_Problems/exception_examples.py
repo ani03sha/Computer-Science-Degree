34d293a152c00d10c1d44f22cab230af66b29990
Anirudh Sharma <anirudh03sharma@gmail.com>
2020-01-14 16:19:42 +0530

    Added exception examples

---
+++ 001_IntroductionToCS&ProgrammingUsingPython/Extra_Problems/exception_examples.py
@@ -0,0 +1,41 @@
+"""
+@author: Anirudh Sharma
+"""
+#
+#def exception_examples():
+#    try:
+#        a = int(input("Enter first number: "))
+#        b = int(input("Enter second number: "))
+#        print("a/b:", a/b)
+#        print("a+b:", a+b)
+#    except ValueError:
+#        print("Could not convert to a number")
+#    except ZeroDivisionError:
+#        print("Cannot divide by zero")
+#    except:
+#        print("Something went wrong")
+#    finally:
+#        print("Always executes!!!")
+#        
+#    
+#exception_examples()
+    
+
+def get_ratios(L1, L2):
+    """
+    Assumes: L1 and L2 are lists of equal length
+    Returns: A list containing L1[i]/L2[i]
+    """
+    ratios = []
+    for index in range(len(L1)):
+        try:
+            ratios.append(L1[index] / L2[index])
+        except ZeroDivisionError:
+            ratios.append(float('nan')) # nan - not a number
+        except:
+            raise ValueError("Bad argument occurred")
+    return ratios
+
+L1 = [6, 4, -2, 7]
+L2 = [2, 6, 0, 8]
+print(get_ratios(L1, L2))
