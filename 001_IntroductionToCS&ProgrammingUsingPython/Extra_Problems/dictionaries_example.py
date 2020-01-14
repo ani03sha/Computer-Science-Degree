899dfa66b8d1406c8af20fba02fddd20804817ff
Anirudh Sharma <anirudh03sharma@gmail.com>
2020-01-14 14:06:29 +0530

    Added dictionaries examples

---
+++ 001_IntroductionToCS&ProgrammingUsingPython/Extra_Problems/dictionaries_example.py
@@ -0,0 +1,91 @@
+"""
+@author: Anirudh Sharma
+"""
+
+she_loves_you = ['she', 'loves', 'you', 'yeah', 'yeah', 
+'yeah','she', 'loves', 'you', 'yeah', 'yeah', 'yeah',
+'she', 'loves', 'you', 'yeah', 'yeah', 'yeah',
+
+'you', 'think', "you've", 'lost', 'your', 'love',
+'well', 'i', 'saw', 'her', 'yesterday-yi-yay',
+"it's", 'you', "she's", 'thinking', 'of',
+'and', 'she', 'told', 'me', 'what', 'to', 'say-yi-yay',
+
+'she', 'says', 'she', 'loves', 'you',
+'and', 'you', 'know', 'that', "can't", 'be', 'bad',
+'yes', 'she', 'loves', 'you',
+'and', 'you', 'know', 'you', 'should', 'be', 'glad',
+
+'she', 'said', 'you', 'hurt', 'her', 'so',
+'she', 'almost', 'lost', 'her', 'mind',
+'and', 'now', 'she', 'says', 'she', 'knows',
+"you're", 'not', 'the', 'hurting', 'kind',
+
+'she', 'says', 'she', 'loves', 'you',
+'and', 'you', 'know', 'that', "can't", 'be', 'bad',
+'yes', 'she', 'loves', 'you',
+'and', 'you', 'know', 'you', 'should', 'be', 'glad',
+
+'oo', 'she', 'loves', 'you', 'yeah', 'yeah', 'yeah',
+'she', 'loves', 'you', 'yeah', 'yeah', 'yeah',
+'with', 'a', 'love', 'like', 'that',
+'you', 'know', 'you', 'should', 'be', 'glad',
+
+'you', 'know', "it's", 'up', 'to', 'you',
+'i', 'think', "it's", 'only', 'fair',
+'pride', 'can', 'hurt', 'you', 'too',
+'pologize', 'to', 'her',
+
+'Because', 'she', 'loves', 'you',
+'and', 'you', 'know', 'that', "can't", 'be', 'bad',
+'Yes', 'she', 'loves', 'you',
+'and', 'you', 'know', 'you', 'should', 'be', 'glad',
+
+'oo', 'she', 'loves', 'you', 'yeah', 'yeah', 'yeah',
+'she', 'loves', 'you', 'yeah', 'yeah', 'yeah',
+'with', 'a', 'love', 'like', 'that',
+'you', 'know', 'you', 'should', 'be', 'glad',
+'with', 'a', 'love', 'like', 'that',
+'you', 'know', 'you', 'should', 'be', 'glad',
+'with', 'a', 'love', 'like', 'that',
+'you', 'know', 'you', 'should', 'be', 'glad',
+'yeah', 'yeah', 'yeah',
+'yeah', 'yeah', 'yeah', 'yeah'
+]
+
+def lyrics_to_frequnecies(lyrics):
+    frequencies = {}
+    for word in lyrics:
+        if word in frequencies:
+            frequencies[word] += 1
+        else:
+            frequencies[word] = 1
+    return frequencies
+
+beatles = lyrics_to_frequnecies(she_loves_you)
+
+
+def most_common_word(frequencies):
+    best = max(frequencies.values())
+    words = []
+    for k in frequencies:
+        if frequencies[k] == best:
+            words.append(k)
+    return (words, best)
+
+
+def most_often(frequencies, minimumTimes):
+    result = []
+    flag = False
+    while not flag:
+        temp = most_common_word(frequencies)
+        if temp[1] >= minimumTimes:
+            result.append(temp)
+            for w in temp[0]:
+                del(frequencies[w])
+        else:
+            flag = True
+    return result
+
+
+print(most_often(beatles, 5))
